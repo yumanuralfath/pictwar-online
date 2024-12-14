d95765c0f41e0c7da2e2b19cfe095236cfc439ce
yumanuralfath <yumanuralfath2@gmail.com>
2024-12-14 18:13:53 +0700

    migration: add file migration

---
+++ migrations/2024-12-14-111037_add_column_to_users/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+ALTER TABLE users DROP COLUMN is_admin;
+ALTER TABLE users DROP COLUMN created_at;
+ALTER TABLE users DROP COLUMN updated_at;
+ALTER TABLE users DROP COLUMN password;
